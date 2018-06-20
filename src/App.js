e7f25418434f518351e1aa90b05ae135bb3872bc
Ed Brett <e.j.a.brett@gmail.com>
2018-06-20 20:04:14 +0200

    init legend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import { LayerManagerLeaflet, LayerManagerGmaps } from 'layer-manager';
-import wriSerializer from 'wri-json-api-serializer';
+import isEmpty from 'lodash/isEmpty';
+
+import {
+  // Legend
+  Legend,
+
+  // Toolbar
+  LegendItemToolbar,
+  LegendItemButtonBBox,
+  LegendItemButtonLayers,
+  LegendItemButtonOpacity,
+  LegendItemButtonVisibility,
+  LegendItemButtonInfo,
+  LegendItemButtonRemove,
+
+  // Types
+  LegendItemTypes,
+  LegendItemTypeBasic,
+  LegendItemTypeChoropleth,
+  LegendItemTypeGradient,
+  LegendItemTypeProportional
+
+} from 'wri-api-components';
 
 import './App.css';
 const L = window.L;
 const google = window.google;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      layerSpec: {}
+    };
+  }
   componentDidMount() {
     this.initMap();
     this.layerManager = new LayerManagerLeaflet(this.map, {
@@ -18,13 +46,19 @@ class App extends Component {
     fetch('https://api.resourcewatch.org/v1/layer/0ac7bf69-388a-48b0-a869-c3240031c4bf?application=rw&provider=cartodb')
       .then(response => response.json())
       .then(layerSpec => {
-
-        this.layerManager.add({ data: [layerSpec.data] });
+        this.setState({ layerSpec });
       })
   }
 
+  componentWillUpdate(nextProps, nextState) {
+    const { layerSpec } = nextState;
+    if (!isEmpty(layerSpec)) {
+      this.layerManager.add({ data: [layerSpec.data] });
+    }
+  }
+
   initMap = () => {
-    this.map = L.map('c-map').setView([51.505, -0.09], 3);
+    this.map = L.map('c-map').setView([27, 12], 3);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -36,9 +70,17 @@ class App extends Component {
   }
 
   render() {
+    const { layerSpec } = this.state;
     return (
       <div className="App">
         <div id="c-map"></div>
+        {layerSpec &&
+          <Legend
+            LegendItemToolbar={<LegendItemToolbar />}
+            LegendItemTypes={<LegendItemTypes />}
+            // layerGroups={[layerSpec.data]}
+          />
+        }
       </div>
     );
   }
