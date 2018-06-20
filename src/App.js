40b8504c9804b8ac5ef8aa53435a3dd076b24058
Ed Brett <e.j.a.brett@gmail.com>
2018-06-20 19:27:56 +0200

    init map with layer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { LayerManagerLeaflet, LayerManagerGmaps } from 'layer-manager';
+import wriSerializer from 'wri-json-api-serializer';
+
 import './App.css';
+const L = window.L;
+const google = window.google;
 
 class App extends Component {
+  componentDidMount() {
+    this.initMap();
+    this.layerManager = new LayerManagerLeaflet(this.map, {
+      serialize: true
+    });
+    // this.layerManager = new LayerManagerGmaps(this.map, {
+    //   serialize: true
+    // });
+    fetch('https://api.resourcewatch.org/v1/layer/0ac7bf69-388a-48b0-a869-c3240031c4bf?application=rw&provider=cartodb')
+      .then(response => response.json())
+      .then(layerSpec => {
+
+        this.layerManager.add({ data: [layerSpec.data] });
+      })
+  }
+
+  initMap = () => {
+    this.map = L.map('c-map').setView([51.505, -0.09], 3);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
+    // this.map = new google.maps.Map(document.getElementById('c-map'), {
+    //   center: {lat: -34.397, lng: 150.644},
+    //   zoom: 8
+    // });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id="c-map"></div>
       </div>
     );
   }
