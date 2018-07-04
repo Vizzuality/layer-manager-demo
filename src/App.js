9d56880450b05bd34a6bc6f402683b6f9ed1820b
Ed Brett <e.j.a.brett@gmail.com>
2018-07-04 18:22:25 +0200

    add mapbox gl react to page and link with state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { LayerManagerLeaflet } from 'layer-manager';
 import wriAPISerializer from 'wri-json-api-serializer';
 import flatten from 'lodash/flatten';
+import ReactMapGL from 'react-map-gl';
 
 import {
   Legend,
@@ -22,7 +23,14 @@ class App extends Component {
     this.state = {
       layerGroups: [],
       datasets: [],
-      apiUrl: 'https://api.resourcewatch.org/v1/dataset?application=rw&includes=metadata,vocabulary,layer&page[size]=1'
+      apiUrl: 'https://api.resourcewatch.org/v1/dataset?application=rw&includes=metadata,vocabulary,layer&page[size]=1',
+      viewport: {
+        width: 1000,
+        height: 1000,
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 8
+      }
     };
   }
   componentDidMount() {
@@ -30,15 +38,11 @@ class App extends Component {
     this.layerManager = new LayerManagerLeaflet(this.map, {
       serialize: false
     });
-    this.getLayer(this.state.apiUrl);
+    // this.getLayer(this.state.apiUrl);
   }
 
   componentWillUpdate(nextProps, nextState) {
     if (nextState.datasets !== this.state.datasets) {
-      
-      
-
-  
       this.layerManager.add(flatten(nextState.datasets.map(d => d.layer)), {
         opacity: 1,
         visibility: true,
@@ -85,16 +89,18 @@ class App extends Component {
   }
 
   initMap = () => {
-    this.map = L.map('c-map', {
-      zoomControl: false
-    }).setView([27, 12], 3);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(this.map);
-    L.control.zoom({
-      position:'topright'
-    }).addTo(this.map);
+    this.map = this.mapRef.getMap();
+    console.log(this.map);
+    // this.map = L.map('c-map', {
+    //   zoomControl: false
+    // }).setView([27, 12], 3);
+    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   maxZoom: 19,
+    //   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    // }).addTo(this.map);
+    // L.control.zoom({
+    //   position:'topright'
+    // }).addTo(this.map);
   }
 
   render() {
@@ -102,12 +108,18 @@ class App extends Component {
     return (
       <div className="App">
         <Icons />
-        <div id="c-map" onClick={
+        <ReactMapGL
+          ref={el => { this.mapRef= el }}
+          mapboxApiAccessToken={'pk.eyJ1IjoiZWRicmV0dCIsImEiOiJjaXVpZjI4dDkwMDJ3MnRxbGJhaG05bXV2In0.i-xl06neUzmzlxi9EPBfjA'}
+          {...this.state.viewport}
+          onViewportChange={(viewport) => this.setState({viewport})}
+        />
+        {/* <div id="c-map" onClick={
           () => {
             console.log(this.layerManager.find('e1dc5626-c1c2-4d60-a6a9-746a33fe1cb7'));
             this.layerManager.find('e1dc5626-c1c2-4d60-a6a9-746a33fe1cb7').setOpacity(0);
           }
-        }></div>
+        }></div> */}
         <form onSubmit={this.handleSubmit}>
           <input placeholder="Enter and RW API url..." type="text" className="input" value={this.state.apiUrl} onChange={this.setValue} />
         </form>
