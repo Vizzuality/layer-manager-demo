1ca2cba39cd19ff562c33a23f833570efe22c5f2
Ed Brett <e.j.a.brett@gmail.com>
2018-07-10 19:23:04 +0200

    remove hash

--- src/components/search/reducers.js
+++ src/components/search/reducers.js
@@ -1,5 +1,5 @@
 export const initialState = {
-    apiUrl: 'https://api.resourcewatch.org/v1/dataset?application=rw&includes=layer&page[size]=1&hash=dsadsadsadsadsa'
+    apiUrl: 'https://api.resourcewatch.org/v1/dataset?application=rw&includes=layer&page[size]=1'
   };
   
   const setUrl = (state, { payload }) => ({
