9e51aa45f195753c62becd0faaefd5e561e9a41d
Ed Brett <e.j.a.brett@gmail.com>
2018-07-30 18:44:15 +0200

    dataset menu changing layers on the map

--- src/components/search/reducers.js
+++ src/components/search/reducers.js
@@ -1,5 +1,5 @@
 export const initialState = {
-  apiUrl: 'https://api.resourcewatch.org/v1/dataset/e663eb09-04de-4f39-b871-35c6c2ed10b5?includes=layer&application=gfw&hash=4243312312231'
+  apiUrl: 'https://api.resourcewatch.org/v1/dataset/81c802aa-5feb-4fbe-9986-8f30c0597c4d?includes=layer&application=gfw&hash=4243312312231'
   };
 
   const setUrl = (state, { payload }) => ({
